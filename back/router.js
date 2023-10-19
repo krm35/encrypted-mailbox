c4a85d60a6797116f121b55bf97d4f1cb8b8b596
krm35 <etcc@protonmail.com>
2023-10-19 10:04:11 +0200

    Use readdirSync

--- back/router.js
+++ back/router.js
@@ -8,19 +8,18 @@ router['files'] = {};
 module.exports = router;
 
 function loadFiles(path, module) {
-    fs.readdir(path, function (err, files) {
-        for (let f in files) {
-            if (fs.lstatSync(path + files[f]).isDirectory()) {
-                loadFiles(path + files[f] + "/", module);
+    const files = fs.readdirSync(path);
+    for (let f in files) {
+        if (fs.lstatSync(path + files[f]).isDirectory()) {
+            loadFiles(path + files[f] + "/", module);
+        } else {
+            if (module) {
+                if (files[f].endsWith('.js') || files[f].endsWith('.jsc')) require('./' + path + files[f]);
             } else {
-                if (module) {
-                    if (files[f].endsWith('.js') || files[f].endsWith('.jsc')) require('./' + path + files[f]);
-                } else {
-                    router['files'][files[f]] = !c.cache ? () => fs.readFileSync(path + files[f]) : fs.readFileSync(path + files[f]);
-                }
+                router['files'][files[f]] = !c.cache ? () => fs.readFileSync(path + files[f]) : fs.readFileSync(path + files[f]);
             }
         }
-    });
+    }
 }
 
 loadFiles('services/', true);
