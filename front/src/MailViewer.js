cbb7fa9c0d34b731987e26a856eb88f353965557
krm35 <etcc@protonmail.com>
2024-01-15 11:01:52 +0100

    Add linkify only if there is no href

--- front/src/MailViewer.js
+++ front/src/MailViewer.js
@@ -34,8 +34,7 @@ export default function MailViewer(props) {
                 <div
                     id={"decrypted"}
                     dangerouslySetInnerHTML={{
-                        __html: DOMPurify.sanitize(raw ? JSON.stringify(mail.decrypted) : mail.textAsHtml)
-                            // .replace(/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig, (url) => '<a href="' + url + '" target="_blank">' + url + '</a>')
+                        __html: linkify(DOMPurify.sanitize(raw ? JSON.stringify(mail.decrypted) : mail.textAsHtml))
                     }}
                 />
             }
@@ -80,6 +79,13 @@ export default function MailViewer(props) {
             </div>
         </div>
     </Dialog>
+}
+
+const urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+
+function linkify(text) {
+    if (text.includes('href=')) return text;
+    return text.replace(urlRegex, (url) => '<a href="' + url + '">' + url + '</a>');
 }
 
 function forwardHeader(mail) {
