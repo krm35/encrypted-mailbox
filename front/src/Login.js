f43926a7a784693da90507181d552c1f78d9e256
krm35 <etcc@protonmail.com>
2024-03-14 13:52:09 +0100

    Remove autologin query param check

--- front/src/Login.js
+++ front/src/Login.js
@@ -27,7 +27,7 @@ export default function Login(props) {
     useEffect(() => {
         setTimeout(function () {
             try {
-                if (!window.location.search.includes("?autologin") || !document.getElementById("password").value.length || !props.autologin) return;
+                if (!document.getElementById("password").value.length || !props.autologin) return;
                 document.getElementById("sign-in")?.click();
             } catch (e) {
             }
